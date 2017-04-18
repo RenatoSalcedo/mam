bf23e7089a9e3969102511112072d74d13d1671b
Renato Salcedo <renato_salcedo@hotmail.com>
2017-04-18 10:08:48 -0300

    mudança em barra de navegação.

--- ClientApp/app/components/navmenu/navmenu.component.ts
+++ ClientApp/app/components/navmenu/navmenu.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'nav-menu',
     templateUrl: './navmenu.component.html',
-    styleUrls: ['../../Css/bootstrap.css', '../../Css/scrolling-nav.css', '../../Css/custom.css']
+    styleUrls: ['../../Css/bootstrap.css', '../../Css/scrolling-nav.css', '../../Css/custom.css', '../../Css/navbar.css']
 })
 export class NavMenuComponent {
 }
