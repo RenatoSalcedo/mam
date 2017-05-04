3317433c4a679ef31b6d30211f8690d74db3bf8e
Renato Salcedo <renato_salcedo@hotmail.com>
2017-05-04 08:26:00 -0300

    ajustes de layout em geral.

--- ClientApp/app/admin/login/login.component.ts
+++ ClientApp/app/admin/login/login.component.ts
@@ -5,5 +5,6 @@ import { Component } from '@angular/core';
     templateUrl: './login.component.html',
     styleUrls: ['../../Css/login.css', '../../Css/w3.css']
 })
+
 export class LoginComponent {
 }
---
+++ ClientApp/app/admin/produto/cadastro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'cadastro',
+    templateUrl: './cadastro.component.html',
+    styleUrls: ['../../Css/login.css', '../../Css/w3.css']
+})
+export class CadastroComponent {
+}
